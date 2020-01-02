1a4e82dc9ed7dcf249243ee8fe687bf4daba1941
Graham Wihlidal <graham@wihlidal.ca>
2020-01-02 02:14:17 -0700

    Passing test: enumerate_sets

--- tests/mod.rs
+++ tests/mod.rs
@@ -9,15 +9,19 @@ mod tests {
         let ps_data = include_bytes!("./ImGuiPs.spirv");
         ShaderModule::load_u8_data(ps_data).unwrap();
     }
-    /*
+
     #[test]
     fn enumerate_sets() {
         let ps_data = include_bytes!("./ImGuiPs.spirv");
-        let _module = ShaderModule::load_u8_data(ps_data).unwrap();
+        let module = ShaderModule::load_u8_data(ps_data).unwrap();
+        let descriptor_sets = module.enumerate_descriptor_sets(None).unwrap();
 
-        //
+        assert_eq!(descriptor_sets.len(), 1);
+        let descriptor_set = &descriptor_sets[0];
+        assert_eq!(descriptor_set.bindings.len(), 2);
     }
 
+    /*
     #[test]
     fn enumerate_bindings() {
         let ps_data = include_bytes!("./ImGuiPs.spirv");
